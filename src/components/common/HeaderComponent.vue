a5fb9ff7a62f6d2cbf391e60cf0780f4a2797180
DionisPascaru <denis.pascaru95@gmail.com>
2022-10-13 23:37:34 +0300

    Style home view, refactor menu in two separated components

--- src/components/common/HeaderComponent.vue
+++ src/components/common/HeaderComponent.vue
@@ -1,70 +1,34 @@
 <template>
     <div class="container header">
-        <div class="nav-header">
-            <div class="hamburger-lines" 
-                :class="{active: this.isActive}"
-                @click="this.collabseMenu">
-                <span class="line"></span>
-                <span class="line"></span>
-                <span class="line"></span>
-            </div>
-            <logo-component></logo-component>
-            <dark-light-mode-component></dark-light-mode-component>
-        </div>
-        <div class="nav-wrapper" :class="{active: this.isActive}">
-            <el-menu
-                :default-active="activeMenuItem"
-                class="nav-menu-items"
-                :router="true">
-                <el-menu-item index="AboutView" :route="{ name: 'AboutView'}" @click="this.collabseMenu">
-                    <span>About us</span>
-                </el-menu-item>
-                <el-menu-item index="ServicesView" :route="{ name: 'ServicesView'}" @click="this.collabseMenu">
-                    <span>Our services</span>
-                </el-menu-item>
-                <el-menu-item index="TechnologiesView" :route="{ name: 'TechnologiesView'}" @click="this.collabseMenu">
-                    <span>Technologies</span>
-                </el-menu-item>
-                <el-menu-item index="PortfolioView" :route="{ name: 'PortfolioView'}" @click="this.collabseMenu">
-                    <span>Portfolio</span>
-                </el-menu-item>
-                <el-menu-item index="ContactView" :route="{ name: 'ContactView'}" @click="this.collabseMenu">
-                    <span>Contact</span>
-                </el-menu-item>
-            </el-menu>
-            <div class="nav-footer">
-                <p>If you have any questions, don’t hesitate to call us or send an email to the email address. We are ready to answer your calls!</p>
-                <h2>+373 69 762 235</h2>
-                <span>webstudiose@gmail.com</span>
-            </div>
-        </div>
+        <menu-component v-if="!this.isMobile"></menu-component>
+        <mobile-menu-component v-if="this.isMobile"></mobile-menu-component>
     </div>
 </template>
 
 <script>
-import LogoComponent from '../common/LogoComponent.vue';
-import DarkLightModeComponent from '../common/DarkLightModeComponent.vue';
+import MenuComponent from '../common/MenuComponent.vue';
+import MobileMenuComponent from '../common/MobileMenuComponent.vue';
 
 export default {
     name: 'HeaderComponent',
     components: {
-        LogoComponent,
-        DarkLightModeComponent,
+        MenuComponent,
+        MobileMenuComponent
     },
     data() {
         return {
-            isActive: false
+            windowWidth: window.innerWidth,
         }
     },
     computed: {
-        activeMenuItem() {
-            return this.$route.name;
-        },
+        isMobile() {
+            return this.windowWidth < 992;
+        }
     },
-    methods: {
-        collabseMenu() {
-            this.isActive = !this.isActive;
-        },
-    }
+    mounted() {
+        window.addEventListener("resize", () => {
+            this.windowWidth = window.innerWidth;
+        });
+    },
 }
 </script>
---
+++ src/components/common/MenuComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div class="nav-menu-desktop">
+            <div class="nav-menu-left">
+                <logo-component></logo-component>
+            </div>
+            <div class="nav-menu-right">
+                <el-menu
+                    :default-active="activeMenuItem"
+                    class="nav-menu-items"
+                    :router="true">
+                    <el-menu-item index="AboutView" :route="{ name: 'AboutView'}">
+                        <span>About us</span>
+                    </el-menu-item>
+                    <el-menu-item index="ServicesView" :route="{ name: 'ServicesView'}">
+                        <span>Our services</span>
+                    </el-menu-item>
+                    <el-menu-item index="TechnologiesView" :route="{ name: 'TechnologiesView'}">
+                        <span>Technologies</span>
+                    </el-menu-item>
+                    <el-menu-item index="PortfolioView" :route="{ name: 'PortfolioView'}">
+                        <span>Portfolio</span>
+                    </el-menu-item>
+                    <el-menu-item index="ContactView" :route="{ name: 'ContactView'}">
+                        <span>Contact</span>
+                    </el-menu-item>
+                </el-menu>
+                <dark-light-mode-component></dark-light-mode-component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import LogoComponent from '../common/LogoComponent.vue';
+import DarkLightModeComponent from '../common/DarkLightModeComponent.vue';
+
+export default {
+    name: 'MenuComponent',
+    components: {
+        LogoComponent,
+        DarkLightModeComponent
+    },
+    computed: {
+        activeMenuItem() {
+            return this.$route.name;
+        },
+    }
+}
+</script>
