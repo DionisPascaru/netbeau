eb0037a9a212381e07b06939e22278b066e82354
DionisPascaru <denis.pascaru95@gmail.com>
2022-10-18 22:13:51 +0300

    Style technologies view

---
+++ src/components/common/TechnologiesItemsComponent.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="technologies-items">
+    <div class="items-block">
+      <div class="item" v-for="item in items" :key="item.id">
+        <span>{{ item.name }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TechnologiesItemsComponent',
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          name: 'Python'
+        },
+        {
+          id: 2,
+          name: '.NET'
+        },
+        {
+          id: 3,
+          name: 'PHP'
+        },
+        {
+          id: 4,
+          name: 'ReactJS'
+        },
+        {
+          id: 5,
+          name: 'Angular JS'
+        },
+        {
+          id: 6,
+          name: 'Kotlin'
+        },
+        {
+          id: 7,
+          name: 'Swift'
+        },
+        {
+          id: 8,
+          name: 'C / C++ / C#'
+        },
+        {
+          id: 9,
+          name: 'L.A.M.P.'
+        },
+        {
+          id: 10,
+          name: 'Unity'
+        },
+        {
+          id: 11,
+          name: 'Laravel'
+        },
+        {
+          id: 12,
+          name: 'Django'
+        },
+      ]
+    }
+  }
+}
+</script>
--- src/views/TechnologiesView.vue
+++ src/views/TechnologiesView.vue
@@ -1,11 +1,56 @@
 <template>
-  <div>
-    <h1>Technologies</h1>
+  <div class="technologies">
+    <div class="container">
+      <div class="page-heading">
+        <h1>Technologies</h1>
+      </div>
+      <section>
+        <div class="section border-bottom">
+          <el-row :gutter="20">
+            <el-col :lg="8">
+              <p class="text-normal">A 100% commitment to building trust and transparency.</p>
+            </el-col>
+            <el-col :lg="16">
+              <div class="technologies-layout">
+                <p class="text-dingy">Developing high-quality digital products requires trust and transparency. </p>
+                <div class="technologies-column">
+                  <p class="text-deep">That’s what we work with each customer to define mutual account abilities and
+                    focus the whole team on “getting the job done.” We call this our Collaboration Framework, a
+                    continuously evolving set of good practices that meet and exceed industry standards.</p>
+                </div>
+                <div class="technologies-column">
+                  <p class="text-deep">By establishing high-performing collaborations with customers, we’ve learned how
+                    to increase velocity (the delivery of project value) while reducing the waste that can impede
+                    successful project delivery.</p>
+                </div>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+      </section>
+      <section>
+        <div class="section">
+          <el-row :gutter="20">
+            <el-col :lg="8">
+              <p class="text-normal">Technologies we use in our work</p>
+            </el-col>
+            <el-col :lg="16">
+              <technologies-items-component></technologies-items-component>
+            </el-col>
+          </el-row>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
+import TechnologiesItemsComponent from "@/components/common/TechnologiesItemsComponent";
+
 export default {
-  name: 'TechnologiesView'
+  name: 'TechnologiesView',
+  components: {
+    TechnologiesItemsComponent
+  }
 }
 </script>
