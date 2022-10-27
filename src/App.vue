a94a867068fa710bdf4d8d4414b3252badfec78f
DionisPascaru <denis.pascaru95@gmail.com>
2022-10-28 00:13:04 +0300

    Create move up button

--- src/App.vue
+++ src/App.vue
@@ -11,18 +11,21 @@
         <footer-component></footer-component>
       </el-footer>
     </el-container>
+    <move-up-component></move-up-component>
   </div>
 </template>
 
 <script>
 import HeaderComponent from './components/header/HeaderComponent.vue';
 import FooterComponent from "@/components/footer/FooterComponent";
+import MoveUpComponent from "@/components/common/MoveUpComponent";
 
 export default {
   name: 'App',
   components: {
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MoveUpComponent
   }
 }
 </script>
